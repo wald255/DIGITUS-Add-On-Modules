edd43d7061e0d191825fdd6def768316d26e5454
Eigene Datei <waldemar65@hotmail.de>
2026-04-04 10:03:03 +0200

    ##

--- enc_temp_folder/6a7a959c1c8f684fe72e8720a3385ed3/Program.cs
+++ enc_temp_folder/6a7a959c1c8f684fe72e8720a3385ed3/Program.cs
@@ -1,6 +1,10 @@
 ﻿namespace DIGITUS_Add_On_Modules
 {
+<<<<<<< HEAD
     internal class Program ALSCAL_Program
+=======
+    internal class Program
+>>>>>>> 41b8350 (Projektdateien hinzufügen.#)
     {
         static void Main(string[] args)
         {
