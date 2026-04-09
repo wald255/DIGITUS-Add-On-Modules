679b02b7b2ecaa999ef43eb4e697fd6c48e031f0
Eigene Datei <waldemar65@hotmail.de>
2026-04-09 16:33:10 +0200

    #

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿namespace DIGITUS_Add_On_Modules
+﻿using System.Reflection.Emit;
+
+namespace DIGITUS_Add_On_Modules
 {
-    internal class Program
+    internal class Program  41b8350f0a7d0a64a55dd22a5307dc17d3c18f59
+                STATS TABLE CALC /HELP.
     {
         static void Main(string[] args)
         {
